9da424979215342340f9877d8182351aa3c8d608
Muntajir Hussaingazi <muntajirwork11@gmail.com>
2024-04-29 22:33:13 +0530

    Login Signup Frontend Update

---
+++ Frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import GenderCheckbox from './genderCheckBox';
+
+const SignUp = () => {
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> ChatiFY💭</span>
+				</h1>
+
+				<form>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base text-emerald-950 label-text'>Full Name</span>
+						</label>
+						<input type='text' placeholder='Full Name' className='w-full input input-bordered  h-10' />
+					</div>
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base text-emerald-950 label-text'>Username</span>
+						</label>
+						<input type='text' placeholder='Username' className='w-full input input-bordered h-10' />
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base text-emerald-950 label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base text-emerald-950 label-text'>Confirm Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Confirm Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+          
+					<GenderCheckbox />
+
+					<a className='text-sm hover:underline text-emerald-950 hover:text-blue-600 mt-2 inline-block' href='#'>
+						Already have an account?
+					</a>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default SignUp;
