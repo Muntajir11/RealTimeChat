14e659eb5794caf8c64296147fa31f15a9bb8b4f
Muntajir Hussaingazi <muntajirwork11@gmail.com>
2024-05-22 22:32:30 +0530

    deployement

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,35 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-
-import { app, server } from "./socket/socket.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-
-
-server.listen(PORT,() => {
-    connectToMongoDB();
-    console.log(`Server running on port ${PORT}`);
-
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
