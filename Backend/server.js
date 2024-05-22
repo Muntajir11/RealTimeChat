99ca09dc3f420023d4ef5c9aac18c431f60a294f
Muntajir Hussaingazi <muntajirwork11@gmail.com>
2024-05-22 23:05:19 +0530

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -23,10 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
